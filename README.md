f6ccf4f6abaa133f1dd8369d6376f92720701680
Rekord <1324596506@qq.com>
2022-05-25 19:29:12 +0800

    feat: limit the size of log file

--- README.md
+++ README.md
@@ -88,7 +88,7 @@ python crawler/clean_sn.py
 - [ ] 细化异常处理
 - [x] 提供运行日志记录
 - [x] 将作业打印为`md`格式
-- [ ] 保证[info.log](info.log)文件不至于过大
+- [x] 保证[info.log](info.log)文件不至于过大(限制最大为196kB)
 
 ## 参考
 - [aahnik/bulk-email-sender](https://github.com/aahnik/bulk-email-sender)
--- update_log.sh
+++ update_log.sh
@@ -11,3 +11,17 @@ if [ -a info.log ]; then
 fi
 
 mv info.bak.log info.log
+
+# get the size of the log file
+size=`wc -c info.log | cut -f 1 -d " "`
+# limit the size of the log file to 196kb
+if [ $size -ge 196000 ]; then
+    # delete half of the content
+    line=`wc -l info.log | cut -f 1 -d " "`
+    line=$(($line / 2))
+
+    for i in $(seq 1 $line); do
+        sed -i '$d' info.log;
+    done;
+    echo "The log file is too large, delete half of the content."
+fi
